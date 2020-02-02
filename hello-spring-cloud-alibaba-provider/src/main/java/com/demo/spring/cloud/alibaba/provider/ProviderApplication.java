28c73ccc010c00cd92f54ec78bd9650164534900
hellomyheart <31239692+hellomyheart@users.noreply.github.com>
2020-02-02 16:31:37 +0800

    init commit

--- hello-spring-cloud-alibaba-provider/src/main/java/com/demo/spring/cloud/alibaba/provider/ProviderApplication.java
+++ hello-spring-cloud-alibaba-provider/src/main/java/com/demo/spring/cloud/alibaba/provider/ProviderApplication.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ProviderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProviderApplication.class,args);
+        SpringApplication.run(ProviderApplication.class, args);
     }
 }
